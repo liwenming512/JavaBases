2f54099c8c43d6d0df293b237525ee60959bebf9
liwenming <liwenming@ane56.com>
2019-11-13 13:47:12 +0800

    尝试网上提供的泛型typehandler，失败。

--- JavaDemos/mybatisdemo/src/main/java/com/mybatis/demo/model/OrgTypeEnum.java
+++ JavaDemos/mybatisdemo/src/main/java/com/mybatis/demo/model/OrgTypeEnum.java
@@ -1,57 +1,29 @@
 package com.mybatis.demo.model;
 
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Created by Administrator on 2019/11/11.
+ * Created by Administrator on 2019/11/13.
  */
-public enum OrgTypeEnum {
+public enum OrgTypeEnum implements Translatable {
 
     FENBO(11601, "分拨"),
     WANGDIAN(11603, "网点"),
     GONGSI(11602, "公司");
 
-    private Integer key;
-    private String value;
-    private static Map<Integer, OrgTypeEnum> orgTypeEnumMap = new HashMap<>();
+    private String nameCn;
+    private int code;
 
-    public Integer getKey() {
-        return key;
+    OrgTypeEnum(int code, String nameCn){
+        this.code = code;
+        this.nameCn = nameCn;
     }
 
-    public void setKey(Integer key) {
-        this.key = key;
+    @Override
+    public Integer getCode() {
+        return this.code;
     }
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    OrgTypeEnum(Integer key, String value){
-        this.key = key;
-        this.value = value;
-    }
-
-    /**
-     *
-     * @Title: getSexEnumByKey
-     * @Description: 依据key获取枚举
-     * @param key
-     * @return
-     */
-    public static OrgTypeEnum getSexEnumByKey(Integer key) {
-        return orgTypeEnumMap.get(key);
-    }
-
-    static {
-        for (OrgTypeEnum sexEnum : OrgTypeEnum.values()) {
-            orgTypeEnumMap.put(sexEnum.getKey(), sexEnum);
-        }
+    @Override
+    public String getNameCn() {
+        return this.nameCn;
     }
 }
---
+++ JavaDemos/mybatisdemo/src/main/java/com/mybatis/demo/model/Translatable.java
@@ -0,0 +1,11 @@
+package com.mybatis.demo.model;
+
+/**
+ * Created by Administrator on 2019/11/13.
+ */
+public interface Translatable {
+
+    Integer getCode();
+
+    String getNameCn();
+}
