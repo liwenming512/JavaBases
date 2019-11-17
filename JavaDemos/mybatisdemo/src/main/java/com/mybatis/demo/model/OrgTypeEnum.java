f6239ea2ce4ca9e145dfd490fb6c29e46dfb7d4e
liwenming <liwenming@ane56.com>
2019-11-17 19:45:55 +0800

    去除提交时代码冲突的部分。

--- JavaDemos/mybatisdemo/src/main/java/com/mybatis/demo/model/OrgTypeEnum.java
+++ JavaDemos/mybatisdemo/src/main/java/com/mybatis/demo/model/OrgTypeEnum.java
@@ -3,31 +3,12 @@ package com.mybatis.demo.model;
 /**
  * Created by Administrator on 2019/11/13.
  */
-public enum OrgTypeEnum implements Translatable {
+public enum OrgTypeEnum {
 
     FENBO("分拨"),
     WANGDIAN("网点"),
     GONGSI("公司");
 
-<<<<<<< Updated upstream
-    private String nameCn;
-    private int code;
-
-    OrgTypeEnum(int code, String nameCn){
-        this.code = code;
-        this.nameCn = nameCn;
-    }
-
-    @Override
-    public Integer getCode() {
-        return this.code;
-    }
-
-    @Override
-    public String getNameCn() {
-        return this.nameCn;
-    }
-=======
     private String value;
 
     public String getValue() {
@@ -41,5 +22,4 @@ public enum OrgTypeEnum implements Translatable {
     OrgTypeEnum(String value){
         this.value = value;
     }
->>>>>>> Stashed changes
 }
